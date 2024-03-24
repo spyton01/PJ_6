293179362696bbb0485e70c2dfea132c02bb5455
Awirut Kongsila <160513323+spyton01@users.noreply.github.com>
2024-03-24 14:28:00 -0500

    commit
    changed geometry from gird to place

--- place.py
+++ place.py
@@ -10,23 +10,23 @@ username=tk.StringVar()
 password=tk.StringVar()
 
 
-#label description
+#label description display
 name_label = tk.Label(root, text='Username')
 name_entry = tk.Entry(root, textvariable=username)
 
 password_label = tk.Label(root, text='Password')
 password_entry = tk.Entry(root, textvariable=password, show = '*')
 
-#click bttn
 sub_btn = tk.Button(root, text='Login')
 
-name_label.grid(row=0, column=0)
-name_entry.grid(row=0, column=1)
+#geometry
+name_label.place(x=30, y=50)
+name_entry.place(x=120, y=50)
 
-password_label.grid(row=1, column=0)
-password_entry.grid(row=1, column=1)
+password_label.place(x=30, y=80)
+password_entry.place(x=120, y=80)
 
-sub_btn.grid(row=2, columnspan=2)
+sub_btn.place(x=130, y=120)
 
 
 
