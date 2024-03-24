30dd3333018fbfe17d339d22dc46ce588527582f
Awirut Kongsila <160513323+spyton01@users.noreply.github.com>
2024-03-24 14:11:21 -0500

    commit
    renamed variable

--- grid.py
+++ grid.py
@@ -8,18 +8,18 @@ root.geometry('300x200')
 
 name=tk.StringVar()
 email=tk.StringVar()
-passw=tk.StringVar()
+password=tk.StringVar()
 
 
-#entry description
+#entry description display
 name_label = tk.Label(root, text='Name')
 name_entry = tk.Entry(root, textvariable=name)
 
 email_label = tk.Label(root, text='Email')
 email_entry = tk.Entry(root, textvariable=email)
 
-passw_label = tk.Label(root, text='Password')
-passw_entry = tk.Entry(root, textvariable=passw, show = '*')
+password_label = tk.Label(root, text='Password')
+password_entry = tk.Entry(root, textvariable=password, show = '*')
 
 #click bttn
 sub_btn = tk.Button(root, text='Sign up Now')
@@ -30,8 +30,8 @@ name_entry.grid(row=0, column=1)
 email_label.grid(row=1, column=0)
 email_entry.grid(row=1, column=1)
 
-passw_label.grid(row=2, column=0)
-passw_entry.grid(row=2, column=1)
+password_label.grid(row=2, column=0)
+password_entry.grid(row=2, column=1)
 
 sub_btn.grid(row=3, columnspan=2)
 
--- place.py
+++ place.py
@@ -7,15 +7,15 @@ root.geometry('300x200')
 
 
 username=tk.StringVar()
-passw=tk.StringVar()
+password=tk.StringVar()
 
 
 #label description
 name_label = tk.Label(root, text='Username')
 name_entry = tk.Entry(root, textvariable=username)
 
-passw_label = tk.Label(root, text='Password')
-passw_entry = tk.Entry(root, textvariable=passw, show = '*')
+password_label = tk.Label(root, text='Password')
+password_entry = tk.Entry(root, textvariable=password, show = '*')
 
 #click bttn
 sub_btn = tk.Button(root, text='Login')
@@ -23,8 +23,8 @@ sub_btn = tk.Button(root, text='Login')
 name_label.grid(row=0, column=0)
 name_entry.grid(row=0, column=1)
 
-passw_label.grid(row=1, column=0)
-passw_entry.grid(row=1, column=1)
+password_label.grid(row=1, column=0)
+password_entry.grid(row=1, column=1)
 
 sub_btn.grid(row=2, columnspan=2)
 
