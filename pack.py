4dd2e6eedb3215679d3e32ccb0d892c66d298607
Awirut Kongsila <160513323+spyton01@users.noreply.github.com>
2024-03-23 22:34:57 -0500

    commit
    make suure when +-*/ the first number disappear before input the next num

--- pack.py
+++ pack.py
@@ -6,25 +6,55 @@ root = Tk()
 e = Entry(root, width=35, borderwidth= 5)
 e.grid(row=0, column=0, columnspan=3, padx= 10, pady= 10)
 
-def button_add():
-    return
+def button_click(number):
+    #e.delete(0, END)
+    current = e.get()
+    e.delete(0, END) 
+    e.insert(0, str(current) + str(number))
 
-button_1 = Button(root, text="1", padx=40, pady=20, command=button_add)
-button_2 = Button(root, text="2", padx=40, pady=20, command=button_add)
-button_3 = Button(root, text="3", padx=40, pady=20, command=button_add)
-button_4 = Button(root, text="4", padx=40, pady=20, command=button_add)
-button_5 = Button(root, text="5", padx=40, pady=20, command=button_add)
-button_6 = Button(root, text="6", padx=40, pady=20, command=button_add)
-button_7 = Button(root, text="7", padx=40, pady=20, command=button_add)
-button_8 = Button(root, text="8", padx=40, pady=20, command=button_add)
-button_9 = Button(root, text="9", padx=40, pady=20, command=button_add)
-button_0 = Button(root, text="0", padx=40, pady=20, command=button_add)
+def button_clear():
+    e.delete(0, END)
+
+def button_add():
+    first_number = e.get()
+    global f_num
+    f_mum = int(first_number)
+    e.delete(0, END)
+
+def button_subtract():
+    first_number = e.get()
+    global f_num
+    f_mum = int(first_number)
+    e.delete(0, END)
+
+def button_time():
+    first_number = e.get()
+    global f_num
+    f_mum = int(first_number)
+    e.delete(0, END)
+
+def button_dvide():
+    first_number = e.get()
+    global f_num
+    f_mum = int(first_number)
+    e.delete(0, END)
+
+button_1 = Button(root, text="1", padx=40, pady=20, command=lambda: button_click(1))
+button_2 = Button(root, text="2", padx=40, pady=20, command=lambda: button_click(2))
+button_3 = Button(root, text="3", padx=40, pady=20, command=lambda: button_click(3))
+button_4 = Button(root, text="4", padx=40, pady=20, command=lambda: button_click(4))
+button_5 = Button(root, text="5", padx=40, pady=20, command=lambda: button_click(5))
+button_6 = Button(root, text="6", padx=40, pady=20, command=lambda: button_click(6))
+button_7 = Button(root, text="7", padx=40, pady=20, command=lambda: button_click(7))
+button_8 = Button(root, text="8", padx=40, pady=20, command=lambda: button_click(8))
+button_9 = Button(root, text="9", padx=40, pady=20, command=lambda: button_click(9))
+button_0 = Button(root, text="0", padx=40, pady=20, command=lambda: button_click(0))
 button_plus = Button(root, text="+", padx=39, pady=20, command=button_add)
-button_minus = Button(root, text="-", padx=39, pady=20, command=button_add)
-button_multiply = Button(root, text="*", padx=39, pady=20, command=button_add)
-button_divide = Button(root, text="/", padx=39, pady=20, command=button_add)
-button_equal = Button(root, text="=", padx=90, pady=20, command=button_add)
-button_clear = Button(root, text="CLEAR", padx=77, pady=20, command=button_add)
+button_minus = Button(root, text="-", padx=39, pady=20, command=button_subtract)
+button_multiply = Button(root, text="*", padx=39, pady=20, command=button_time)
+button_divide = Button(root, text="/", padx=39, pady=20, command=button_dvide)
+button_equal = Button(root, text="=", padx=90, pady=20, command=lambda: button_click())
+button_clear = Button(root, text="CLEAR", padx=77, pady=20, command=button_clear)
 
 
 #put the buttons on screen
