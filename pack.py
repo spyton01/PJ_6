c3c90f5cc3891ca2ecc8723c0f27b538b28cbd3c
Awirut Kongsila <160513323+spyton01@users.noreply.github.com>
2024-03-23 22:51:15 -0500

    commit
    added if statement for the rest of  other math

--- pack.py
+++ pack.py
@@ -18,26 +18,50 @@ def button_clear():
 def button_add():
     first_number = e.get()
     global f_num
-    f_mum = int(first_number)
+    global math
+    math = "plus"
+    f_num = int(first_number)
     e.delete(0, END)
 
 def button_subtract():
     first_number = e.get()
     global f_num
-    f_mum = int(first_number)
+    global math
+    math = "subtract"
+    f_num = int(first_number)
     e.delete(0, END)
 
-def button_time():
+def button_multiply():
     first_number = e.get()
     global f_num
-    f_mum = int(first_number)
+    global math
+    math = "time"
+    f_num = int(first_number)
     e.delete(0, END)
 
-def button_dvide():
+def button_divide():
     first_number = e.get()
     global f_num
-    f_mum = int(first_number)
+    global math
+    math = "divide"
+    f_num = int(first_number)
     e.delete(0, END)
+
+def button_equal():
+    second_num = e.get()
+    e.delete(0, END)
+    
+    if math == "add":
+        e.insert(0, f_num +  int(second_num))
+
+    if math == "subtract":
+        e.insert(0, f_num -  int(second_num))
+
+    if math == "time":
+        e.insert(0, f_num *  int(second_num))
+
+    if math == "divide":
+        e.insert(0, f_num /  int(second_num))
 
 button_1 = Button(root, text="1", padx=40, pady=20, command=lambda: button_click(1))
 button_2 = Button(root, text="2", padx=40, pady=20, command=lambda: button_click(2))
@@ -49,11 +73,12 @@ button_7 = Button(root, text="7", padx=40, pady=20, command=lambda: button_click
 button_8 = Button(root, text="8", padx=40, pady=20, command=lambda: button_click(8))
 button_9 = Button(root, text="9", padx=40, pady=20, command=lambda: button_click(9))
 button_0 = Button(root, text="0", padx=40, pady=20, command=lambda: button_click(0))
-button_plus = Button(root, text="+", padx=39, pady=20, command=button_add)
-button_minus = Button(root, text="-", padx=39, pady=20, command=button_subtract)
-button_multiply = Button(root, text="*", padx=39, pady=20, command=button_time)
-button_divide = Button(root, text="/", padx=39, pady=20, command=button_dvide)
-button_equal = Button(root, text="=", padx=90, pady=20, command=lambda: button_click())
+
+button_add = Button(root, text="+", padx=39, pady=20, command=button_add)
+button_subtract = Button(root, text="-", padx=39, pady=20, command=button_subtract)
+button_multiply = Button(root, text="*", padx=39, pady=20, command=button_multiply)
+button_divide = Button(root, text="/", padx=39, pady=20, command=button_divide)
+button_equal = Button(root, text="=", padx=90, pady=20, command=button_equal)
 button_clear = Button(root, text="CLEAR", padx=77, pady=20, command=button_clear)
 
 
@@ -72,8 +97,8 @@ button_8.grid(row=1, column= 1)
 button_9.grid(row=1, column= 2)
 
 button_0.grid(row=4, column= 0)
-button_plus.grid(row=4, column= 1)
-button_minus.grid(row=4, column= 2)
+button_add.grid(row=4, column= 1)
+button_subtract.grid(row=4, column= 2)
 
 button_equal.grid(row=5, column= 0, columnspan=2)
 button_multiply.grid(row=5, column= 2)
